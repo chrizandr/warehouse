12230be940daeeca5988ed1c97b8a597ca85a1d6
chris <chris.andrew@cai-solutions.com>
2024-07-17 12:12:38 +0000

    new constraints

--- optim.py
+++ optim.py
@@ -1,13 +1,32 @@
-from data import Data
-from tqdm import tqdm
+import logging
+import os
+import warnings
+
 import cvxpy as cp
 import numpy as np
+from tqdm import tqdm
 import xarray as xr
+
+from data import Data
 from largest_gap import LG_Routing
-import warnings
 
 # Suppress all warnings
 warnings.filterwarnings("ignore")
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+handler = logging.FileHandler('output.log')
+handler.setLevel(logging.DEBUG)
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG)
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
+
 
 
 class SRRP:
@@ -35,9 +54,10 @@ class SRRP:
         self.T_star = [0] + self.T
         self.T_dash = self.T + [len(self.T)+1]
 
-        self.b = self.compute_b(load_from="vars/b.nc")
-        self.pi = self.compute_pi(load_from="vars/pi.nc")
-        self.mu = self.compute_mu(load_from="vars/mu.nc")
+        logger.debug("Computing parameters...")
+        self.b = self.compute_b(cache="vars/b.nc")
+        self.pi = self.compute_pi(cache="vars/pi.nc")
+        self.mu = self.compute_mu(cache="vars/mu.nc")
         objective, vars = self.objective()
         constraints = self.constraints(vars)
         breakpoint()
@@ -45,9 +65,12 @@ class SRRP:
         self.process_cycle()
 
 
-    def compute_pi(self, load_from=None, save_to=None):
-        if load_from:
-            pi = xr.open_dataarray(load_from)
+    def compute_pi(self, cache=None):
+        logger.debug("Computing [pi]")
+
+        if cache and os.path.exists(cache):
+            logger.debug("[Using cache]")
+            pi = xr.open_dataarray(cache)
             return pi
 
         pi = xr.DataArray(np.nan * np.ones((len(self.M), len(self.T))),
@@ -67,15 +90,19 @@ class SRRP:
                 if np.isfinite(filled_bikt.sel(k=min_k_index).item()):
                     pi.loc[{'i': i, 't': t}] = min_k_index
                 else:
+                    logger.error("Demand is greater than capacity.....terminating optimization")
                     raise ValueError("Demand is greater than capacity")
-        if save_to:
-            pi.to_netcdf(save_to)
+        if cache:
+            pi.to_netcdf(cache)
 
         return pi
 
-    def compute_mu(self, load_from=None, save_to=None):
-        if load_from:
-            mu = xr.open_dataarray(load_from)
+    def compute_mu(self, cache=None):
+        logger.debug("Computing [mu]")
+
+        if cache and os.path.exists(cache):
+            logger.debug("[Using cache]")
+            mu = xr.open_dataarray(cache)
             return mu
 
         mu = xr.DataArray(np.nan * np.ones((len(self.M), len(self.T_star))),
@@ -96,16 +123,19 @@ class SRRP:
                 if np.isfinite(filled_bikt.sel(t=max_k_index).item()):
                     mu.loc[{'i': i, 't': t}] = max_k_index
                 else:
-                    raise ValueError("Something went wrong")
+                    logger.error("Demand is greater than capacity.....terminating optimization")
+                    raise ValueError("Demand is greater than capacity")
 
-        if save_to:
-            mu.to_netcdf(save_to)
+        if cache:
+            mu.to_netcdf(cache)
         return mu
 
+    def compute_b(self, cache=None):
+        logger.debug("Computing [b]")
 
-    def compute_b(self, load_from=None, save_to=None):
-        if load_from:
-            b = xr.open_dataarray(load_from)
+        if cache and os.path.exists(cache):
+            logger.debug("[Using cache]")
+            b = xr.open_dataarray(cache)
             return b
 
         b = xr.DataArray(np.zeros((len(self.M), len(self.T_star), len(self.T_dash))),
@@ -121,11 +151,10 @@ class SRRP:
                 indices = list(range(k, t))
                 b.loc[:, k, t] = self.r.loc[:, indices].sum(axis=1)
 
-        if save_to:
-            b.to_netcdf(save_to)
+        if cache:
+            b.to_netcdf(cache)
 
         return b
-
 
     def process_cycle(self, apriori_route):
         for i in range(len(self.T)):
@@ -142,6 +171,7 @@ class SRRP:
         return alpha, beta
 
     def objective(self):
+        logger.debug("Constructing [Objective]")
         # Define the predefined sets
         shape_w = (len(self.M), len(self.T_star))
         shape_y = (len(self.M_dash), len(self.M_dash))
@@ -162,13 +192,17 @@ class SRRP:
 
         objective = cp.Minimize(cp.sum(sum(objective_sum)))
 
+        logger.debug("[Objective] constructed")
         return objective, (w, y, z)
 
     def constraints(self, objective_variables):
+        logger.debug("Constructing [Constraints]")
         constraints = []
         c, v = self.constraints_22_to_24_and_37(objective_variables)
+        logger.debug(f"Added {len(c)} new constraints")
         constraints += c
         c, v = self.constraints_25_to_28(objective_variables, v)
+        logger.debug(f"Added {len(c)} new constraints")
         constraints += c
 
     def constraints_22_to_24_and_37(self, objective_variables):
@@ -177,6 +211,7 @@ class SRRP:
 
         I_dash = {1: self.I_dash_init.values[:, None]}
         I = {1: self.I_init.values[:, None]}
+
 
         for t in tqdm(self.T):
             S = cp.multiply(w[t], self.b.loc[:, :, t].values)
@@ -208,14 +243,31 @@ class SRRP:
         I, I_dash = intermediate_variables
         constraints = []
 
-
-        # 25
+        # constraint (25)
         summations = []
         for i in self.M:
             k_range = range(1, int(self.mu.loc[i, 0].item()) + 1)
             summations.append(cp.sum([w[k][i - 1, 0] for k in k_range]))
 
         constraints += [cp.sum(summations) == 1]
+
+
+        # TODO: Check indices and stack the subtractions to reduce number of constraints
+        # constraint (26)
+        for t in tqdm(self.T):
+            a_s, b_s = [], []
+            for i in self.M:
+                k_range_1 = range(t+1, int(self.mu.loc[i, t].item()) + 1)
+                k_range_2 = range(int(self.pi.loc[i, t].item()), t)
+
+                a = cp.sum([w[k][i-1, t] for k in k_range_1])
+                b = cp.sum([w[t][i-1, k] for k in k_range_2])
+                a_s.append(a); b_s.append(b)
+
+            A, B = cp.vstack(a_s), cp.vstack(b_s)
+            constraints += [A - B == 0]
+        return constraints, (I, I_dash)
+        breakpoint()
 
 
 
